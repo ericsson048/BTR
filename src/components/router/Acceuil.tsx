eafa42f16ab37737eaa9ef5e922bc68ce0ec36fc
ericsson048 <eericsson048@email.com>
2024-09-19 16:59:03 +0300

    "Removed React imports from multiple components and deleted calendar.tsx file"

--- src/components/router/Acceuil.tsx
+++ src/components/router/Acceuil.tsx
@@ -1,10 +1,6 @@
-import * as React from "react"
-
-import { Card, CardContent } from "@/components/ui/card"
 import {
   Carousel,
   CarouselContent,
-  CarouselItem,
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel"
--- src/components/router/Portfolio.tsx
+++ src/components/router/Portfolio.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 
 function Portfolio() {
   return (
