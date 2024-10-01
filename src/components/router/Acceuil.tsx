83cf5e9a677337ba2d528c87a0e167a7ae2159b3
ericsson048 <eericsson048@email.com>
2024-10-01 12:43:14 +0300

    "Updated text and styling in Acceuil component"

--- src/components/router/Acceuil.tsx
+++ src/components/router/Acceuil.tsx
@@ -9,7 +9,7 @@ function Acceuil() {
     <main className="flex flex-col items-center justify-center w-full p-0">
        <div className="relative w-full h-[500px]  max-sm:h-[600px] max-[1025px]:h-[500px]">
           <img src={profile} alt="profile" className="object-cover max-sm:blur-[2px] w-full h-full"/>
-          <p className="absolute top-10 left-[50%] right-[50%] translate-x-[-50%] w-[90%]  text-center text-outline italic text-white text-4xl font-[500] max-md:text-4xl max-sm:top-24 max-sm:text-[1.6rem] max-lg:text-3xl max-[400px]:text-xl">Burundi en Temps Réel est une entreprise offrant des solutions web personnalisées, incluant la création de sites, le développement d'applications, l'optimisation, et l'hébergement web.
+          <p className="absolute top-10 left-[50%] right-[50%] translate-x-[-50%] w-[90%]  text-center text-outline italic text-white text-4xl font-[500] max-md:text-4xl max-sm:top-16 max-sm:text-[1.6rem] max-lg:text-3xl max-[400px]:text-xl">Burundi en Temps Réel est une entreprise offrant des solutions web personnalisées, incluant la création de sites, le développement d'applications, l'optimisation, et l'hébergement web.
              Elle se distingue par des services sur mesure adaptés aux besoins de ses clients.</p>
             <div className="absolute right-0 left-0 -bottom-1"> 
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
